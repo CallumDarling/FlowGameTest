8f202fbf9e9e62980b6566121377b7e530775d0d
CallumDarling <37622550+CallumDarling@users.noreply.github.com>
2019-02-06 09:59:02 +0000

    Add project files.

---
+++ FlowGameTest/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FlowGameTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem1
+            // 
+            this.startToolStripMenuItem1.Name = "startToolStripMenuItem1";
+            this.startToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.startToolStripMenuItem1.Text = "Start";
+            this.startToolStripMenuItem1.Click += new System.EventHandler(this.startToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ FlowGameTest/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowGameTest
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        Button[,] btn = new Button[10, 10];       // Create 2D array of buttons
+        //Button[] unkillable = new Button[25];
+        List<Button> unkillable = new List<Button>();
+        Boolean clickHeld = false;
+        Color clickColor = Color.NavajoWhite;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int x = 0; x < btn.GetLength(0); x++)         // Loop for x
+            {
+                for (int y = 0; y < btn.GetLength(1); y++)     // Loop for y
+                {
+                    btn[x, y] = new Button();
+                    btn[x, y].Font = new Font(btn[x, y].Font.FontFamily, 24);
+                    btn[x, y].TextAlign = ContentAlignment.MiddleRight;
+                    btn[x, y].Name = Convert.ToString((x + 1) + "," + (y + 1));
+                    btn[x, y].SetBounds(50 + 46 * x + 1, 50 + 46 * y + 1, 45, 45);
+                    btn[x, y].BackColor = Color.PowderBlue;
+                    //btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
+                    btn[x, y].Click += new EventHandler(this.btnEvent_Click);
+                    btn[x, y].MouseEnter += new EventHandler(this.btnEvent_Enter);
+                    btn[x, y].MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnEvent_Down);
+                    btn[x, y].MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnEvent_Up);
+                    Controls.Add(btn[x, y]);
+                }
+            }
+        }
+        void btnEvent_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(((Button)sender).Text);    // SAME handler as before
+            if (!clickHeld) {
+                clickHeld = true;
+                clickColor = ((Button)sender).BackColor;
+            }
+            else
+            {
+                clickHeld = false;
+            }
+
+            //Color clickColor = ((Button)sender).BackColor;
+        }
+
+        void btnEvent_Down(object sender, EventArgs e)
+        {
+            //((Button)sender).BackColor = Color.Green;
+            //clickHeld = true;
+            //Color clickColor = ((Button)sender).BackColor;
+        }
+        void btnEvent_Up(object sender, EventArgs e)
+        {
+            //clickHeld = false;
+            //Color clickColor = Color.NavajoWhite;
+        }
+
+        void btnEvent_Enter(object sender, EventArgs e)
+        {
+            if (clickHeld && !unkillable.Contains((Button)sender))
+            {
+                ((Button)sender).BackColor = clickColor;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void addFlowPath(int startX, int startY, int endX, int endY, Color col) {
+            Button start = btn[startX, startY];
+            Button end = btn[endX, endY];
+            start.Text = "o";
+            end.Text = "o";
+            unkillable.Add(start);
+            unkillable.Add(end);
+            start.BackColor = col;
+            end.BackColor = col;
+        }
+
+        private string convBoardToString()
+        {
+            string outP = "";
+            for (int x = 0; x < btn.GetLength(0); x++)
+            {
+                for (int y = 0; y < btn.GetLength(1); y++)
+                {
+                    outP += btn[x, y].BackColor.ToString()+";";
+
+
+                }
+                outP += ":";
+            }
+            return "";
+        }
+        private void startToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            addFlowPath(0, 0, 2, 2, Color.Red);
+            
+            
+        }
+    }
+}
