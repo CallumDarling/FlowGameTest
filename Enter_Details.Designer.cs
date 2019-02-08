a86f3c64553894f6bd9d50e6c8dcb33d5bd035e3
sdarby99 <44267771+sdarby99@users.noreply.github.com>
2019-02-08 16:32:48 +0000

    not fully working yet
    could still make it pop up even if it doesnt work to show we tried

---
+++ Enter_Details.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace FlowFreeNew
+{
+    partial class Enter_Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Enter_Details));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Details = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Label();
+            this.namelabel = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.enterdetails = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.nametextbox = new System.Windows.Forms.TextBox();
+            this.scoretextbox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.Details);
+            this.panel1.Controls.Add(this.exit);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(424, 27);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 24);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Details
+            // 
+            this.Details.AutoSize = true;
+            this.Details.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Details.ForeColor = System.Drawing.Color.Black;
+            this.Details.Location = new System.Drawing.Point(42, 4);
+            this.Details.Name = "Details";
+            this.Details.Size = new System.Drawing.Size(56, 20);
+            this.Details.TabIndex = 1;
+            this.Details.Text = "Score";
+            // 
+            // exit
+            // 
+            this.exit.AutoSize = true;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.ForeColor = System.Drawing.Color.Black;
+            this.exit.Location = new System.Drawing.Point(400, 4);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(21, 20);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "X";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // namelabel
+            // 
+            this.namelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namelabel.ForeColor = System.Drawing.Color.Yellow;
+            this.namelabel.Location = new System.Drawing.Point(70, 103);
+            this.namelabel.Name = "namelabel";
+            this.namelabel.Size = new System.Drawing.Size(76, 25);
+            this.namelabel.TabIndex = 10;
+            this.namelabel.Text = "Name:";
+            this.namelabel.Click += new System.EventHandler(this.namelabel_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Yellow;
+            this.panel4.Location = new System.Drawing.Point(414, 27);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 187);
+            this.panel4.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Yellow;
+            this.panel3.Location = new System.Drawing.Point(0, 25);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 189);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Yellow;
+            this.panel2.Location = new System.Drawing.Point(0, 206);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(424, 11);
+            this.panel2.TabIndex = 7;
+            // 
+            // enterdetails
+            // 
+            this.enterdetails.AutoSize = true;
+            this.enterdetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterdetails.ForeColor = System.Drawing.Color.Yellow;
+            this.enterdetails.Location = new System.Drawing.Point(146, 50);
+            this.enterdetails.Name = "enterdetails";
+            this.enterdetails.Size = new System.Drawing.Size(159, 31);
+            this.enterdetails.TabIndex = 6;
+            this.enterdetails.Text = "Your Score";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(70, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Your Score";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BackColor = System.Drawing.Color.Yellow;
+            this.SaveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SaveButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.SaveButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Khaki;
+            this.SaveButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.ForeColor = System.Drawing.Color.Black;
+            this.SaveButton.Location = new System.Drawing.Point(317, 159);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(82, 32);
+            this.SaveButton.TabIndex = 12;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // nametextbox
+            // 
+            this.nametextbox.Location = new System.Drawing.Point(141, 103);
+            this.nametextbox.Name = "nametextbox";
+            this.nametextbox.Size = new System.Drawing.Size(164, 20);
+            this.nametextbox.TabIndex = 13;
+            this.nametextbox.Enter += new System.EventHandler(this.nameLabelEnter);
+            // 
+            // scoretextbox
+            // 
+            this.scoretextbox.Location = new System.Drawing.Point(166, 137);
+            this.scoretextbox.Name = "scoretextbox";
+            this.scoretextbox.Size = new System.Drawing.Size(139, 20);
+            this.scoretextbox.TabIndex = 14;
+            // 
+            // Enter_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(425, 217);
+            this.ControlBox = false;
+            this.Controls.Add(this.scoretextbox);
+            this.Controls.Add(this.nametextbox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namelabel);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.enterdetails);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Enter_Details";
+            this.Text = "Enter_Details";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Details;
+        private System.Windows.Forms.Label exit;
+        private System.Windows.Forms.Label namelabel;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label enterdetails;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox nametextbox;
+        private System.Windows.Forms.TextBox scoretextbox;
+    }
+}
---
+++ Enter_Details.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowFreeNew
+{
+    public partial class Enter_Details : Form
+    {
+        string name = " ";
+        string score = "0";
+        string namescore = " ";
+
+
+        public Enter_Details()
+        {
+            InitializeComponent();
+        }
+
+        private void namelabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void nameLabelEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            namelabel.Text = name;
+            scoretextbox.Text = score;
+            namescore = name + ": " + score;
+
+        }
+
+      }
+}
